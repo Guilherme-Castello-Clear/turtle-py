46536886477ae590a781b9ef050eb6fd7565e009
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-10-29 18:13:35 -0300

    Feat: Extracting colors from images

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
-import random
-import turtle as t
-import heroes
+import colorgram
 
-tim = t.Turtle()
-t.colormode(255)
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
 
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    color = (r, g, b)
-    return color
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
 
-tim.speed("fastest")
-def draw_spirograph(size_of_gap):
-    for _ in range(int(360 / size_of_gap)):
-        tim.color(random_color())
-        tim.circle(100)
-        tim.setheading(tim.heading() + size_of_gap)
-
-
-draw_spirograph(5)
-screen = t.Screen()
-screen.exitonclick()
+print(rgb_colors)
