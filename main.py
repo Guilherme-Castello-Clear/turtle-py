b5c8c30ac8c5ad5ba92f2b4204b8720cc44b6d3a
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-10-29 17:47:21 -0300

    Feat: Drawing different shapes

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import random
 from turtle import Turtle, Screen
 import heroes
 
 tim = Turtle()
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+colors = ["red", "blue", "green"]
+
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+
+for shape_side_n in range(3, 14):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
