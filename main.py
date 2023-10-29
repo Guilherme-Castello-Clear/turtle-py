9d840f61d65c6ec9ba4d5ace139cd0a87edb1b05
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-10-29 17:57:07 -0300

    Feat: Creating random colors

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import random
-from turtle import Turtle, Screen
+import turtle as t
 import heroes
 
-tim = Turtle()
+tim = t.Turtle()
+t.colormode(255)
 
-colors = ["red", "blue", "green"]
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
 directions = [0, 90, 100, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
 for _ in range(200):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
