58a70df05b4ecd23d04c55447e83ae6c5e61c961
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-10-29 17:53:19 -0300

    Feat: Random moves

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ import heroes
 tim = Turtle()
 
 colors = ["red", "blue", "green"]
+directions = [0, 90, 100, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
-
-
-for shape_side_n in range(3, 14):
+for _ in range(200):
     tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
